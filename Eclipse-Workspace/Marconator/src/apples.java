518a6c21e1815b613deef6e659fdccff65d5ba6c
amanovishnu <amanovishnu@gmail.com>
2018-01-10 09:56:46 +0530

    FirstJavaProject

---
+++ Eclipse-Workspace/Marconator/src/apples.java
@@ -0,0 +1,6 @@
+public class apples {
+	public static void main(String[] srgs) {
+		System.out.println("Hello RedHat Developer Program");
+		System.out.println("Java EE Enterprise Edition for Java Developers");
+	}
+}
---
+++ RedHat.java
@@ -0,0 +1,7 @@
+class RedHat{
+	public static void main(String args[]) {
+		System.out.println("RedHat Developer Program");
+		System.out.println("Only for Pro Linux Hackers");
+		System.out.println("And for Aspiring Hackers");
+	}
+}
