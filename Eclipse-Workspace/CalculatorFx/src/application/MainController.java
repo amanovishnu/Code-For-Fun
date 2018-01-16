40e4b45f4d449295b163e0980e251024c8fe2794
amanovishnu <amanovishnu@gmail.com>
2018-01-16 17:49:31 +0530

    Calculator in JavaFx

---
+++ Eclipse-Workspace/CalculatorFx/src/application/MainController.java
@@ -0,0 +1,10 @@
+package application;
+import javafx.event.ActionEvent;
+public class MainController {
+	public void processNumbers(ActionEvent event) {
+		
+	}
+	public void processOperators(ActionEvent event) {
+		
+	}
+}
