0ee6237760823a57fceab5a77c65ce497dd29a01
amanovishnu <amanovishnu@gmail.com>
2018-01-19 19:42:30 +0530

    Skeleton File Added

--- Eclipse-Workspace/DemoJavaFx/src/application/Main.java
+++ Eclipse-Workspace/DemoJavaFx/src/application/Main.java
@@ -1,10 +1,13 @@
+package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-public class Skeleton extends Application {
+public class Main extends Application {
 	public static void main(String[] args) {
-		System.out.println("JavaFx Skeleton");
+		System.out.println("Hello World in the Main Application");
 		launch(args);
 	}
 	@Override
@@ -14,14 +17,19 @@ public class Skeleton extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		System.out.println("Inside the Start Method");
+		Button btn = new Button("Click Me");
+		Label lbl = new Label("This is Sparta");
 		StackPane rootnode = new StackPane();
-		Scene myscene = new Scene(rootnode,300,300);
-		primaryStage.setScene(myscene);
+		rootnode.getChildren().add(lbl);
+		rootnode.getChildren().add(btn);
+		//Scene myscene = new Scene(rootnode,300,300);
 		primaryStage.setTitle("JavaFX Skeleton");
+		primaryStage.setScene(new Scene(rootnode,300,300));
 		primaryStage.show();
 	}
 	@Override
 	public void stop() {
-		System.out.println("Inside the Stop Method");
+		System.out.println("Inside Stop Method");
 	}
+
 }
