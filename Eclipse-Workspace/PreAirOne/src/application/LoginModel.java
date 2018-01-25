8bdbce2a01075f2bd1975bd05e41d4e0649e6692
amanovishnu <amanovishnu@gmail.com>
2018-01-25 18:50:49 +0530

    NewProjectCreated

--- Eclipse-Workspace/PreAirOne/src/application/LoginModel.java
+++ Eclipse-Workspace/PreAirOne/src/application/LoginModel.java
@@ -35,10 +35,10 @@ public class LoginModel {
 			
 		} catch (Exception e) {
 			return false;
-		} finally {
+		} /*finally {
 			preparedStatement.close();
 			resultSet.close();
-		}
+		}*/
 	}
 	public boolean isrelogin(String email,String security) throws SQLException {
 		PreparedStatement preparedStatement = null;
@@ -57,13 +57,40 @@ public class LoginModel {
 			
 		} catch (Exception e) {
 			return false;
-		} finally {
+		} /*finally {
 			preparedStatement.close();
 			resultSet.close();
-		}
+		}*/
 	}
 	/****************************************************************************************************************************/
 	public boolean isregister(String name,String username,String email, String password,String securityanswer) throws SQLException {
+		/*PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "INSERT INTO logindata (Name,Username,EmailAddress,Password,SecurityAnswer) VALUES (?,?, ?, ?, ?)";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1,name);
+			preparedStatement.setString(2,username);
+			preparedStatement.setString(3,email);
+			preparedStatement.setString(4,password);
+			preparedStatement.setString(5,securityanswer);
+			resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				int rowsInserted = preparedStatement.executeUpdate();
+				if (rowsInserted > 0) {
+				    System.out.println("A new user was inserted successfully!");
+				}
+				return true;
+			} else {
+				return false;
+			}
+			
+		} catch (Exception e) {
+			return false;
+		} /*finally {
+			preparedStatement.close();
+			resultSet.close();
+		}*/
 		String query = "INSERT INTO logindata (Name,Username,EmailAddress,Password,SecurityAnswer) VALUES (?,?, ?, ?, ?)";
 		try (PreparedStatement pstmt = connection.prepareStatement(query)) {
             pstmt.setString(1, name);
--- JavaFXLoginUI/src/application/Main.java
+++ JavaFXLoginUI/src/application/Main.java
@@ -1,24 +1,28 @@
 package application;
-import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 public class Main extends Application {
-	public static void main(String[] args) {
-		System.out.println("JavaFx Application Started");
-		launch(args);
-	}
 	@Override
-	public void start(Stage primaryStage) throws IOException {
-		
+	public void start(Stage primaryStage) {
+		try {
 			Parent rootnode = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
+			//BorderPane root = new BorderPane();
 			Scene scene = new Scene(rootnode);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("JavaFx Application");
-			primaryStage.show();	
-	}	
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("JavaFx Application Launced");
+		launch(args);
+	}
 }
