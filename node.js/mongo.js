a1d494b2fbc772996c6d20c5974b52e7780d6246
amanovishnu <amanovishnu@gmail.com>
2018-03-17 23:13:09 +0530

    Mongo.js File Updated

--- node.js/mongo.js
+++ node.js/mongo.js
@@ -1,25 +1,12 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/';
-MongoClient.connect(url,function(err,db){
+var  MongoClient = require('mongodb').MongoClient;
+var url ='mongodb://localhost:27017/mydb2';
+MongoClient.connect(url,function(err,res){
 	if(err) throw err;
-	var dbo = db.db('mydb');
-	var myobj = [
-				{name :"company inc", address:"HighWay 37"},
-				{name:"Apple Inc",address:"Cupertino"},
-				{name:"Microsoft Corporation ",address:"Redmond"},
-				{name:"Amazon Systems",address:"California"},
-				{name:"Facebook inc",address:"Silicon valley"},
-				{name:"Pixar Animations",address:"Cupertino"},
-				{name:"Walt Disney Productions",address:"Disneyland"},
-				{name:"twitter Inc",address:"Silicon valley"},
-				{name:"Uber",address:"California"},
-				{name:"Windows Corporation",address:"Redmond"},
-				{name:"IBM Computers",address:"Silicon valley"},
-				{name:"Dell Computers",address:"Deloitte"}
-				];
-	dbo.collection("customers").insertMany(myobj,function(err,res){
+	console.log('Connected to Mongodb');
+	var dbo = db.db('mydb2');
+	dbo.createCollection('Students',function(err,result){
 		if(err) throw err;
-		console.log(res.insertedCount+ 'Document Inserted');
+		console.log('Created Collection Students');
 	});
 	db.close();
 });
