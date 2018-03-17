45c56110fcbf31d71c9e9a8e442d40ad9a65916a
amanovishnu <amanovishnu@gmail.com>
2018-03-17 16:01:50 +0530

    #node Bolerplate Files Removed

---
+++ node.js/app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write('Hello World');
+    res.write('<br>');
+    res.write(req.url);
+    res.end();
+}).listen(8080);
+console.log('Server Running on Port 8080');
