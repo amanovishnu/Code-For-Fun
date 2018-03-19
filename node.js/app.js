22eed04c36f34fa9f7f88c2dc62cd6427744d402
amanovishnu <amanovishnu@gmail.com>
2018-03-19 15:50:02 +0530

    #node files Removed

--- node.js/app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-http.createServer(function(req , res){
-    res.writeHead(200,{'Content-Type':'text/plain','Content-Length':'body_length'});
-    var body = 'My Name is Khan and I am not a Terrorist (Reference to MyName is Khan)';
-    var body_length = body.length;
-    res.write('Hello World');
-    res.end();
-}).listen(3000);
-console.log('Server is Running on Port 3000');
--- node.js/demo.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req,res){
-	res.writeHead(210,{'Content-Type':'text/plain'});
-	res.write('Node.js is a runtime Environment built on Google Chrome JavaScript v8 Engine.');
-	res.end();
-}).listen(8080);
