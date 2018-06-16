60bfbe9c291e498d302d77f85592e95765a21e27
Mano Vishnu <amanovishnu@gmail.com>
2018-06-16 21:25:49 +0530

    Python notes Compleyed

---
+++ Python 3.x/Evangelion.py
@@ -0,0 +1,18 @@
+# python was developed by guido van rossum while he was working at national research institute in netherlands
+'''
+    Tuples are ordered unchangable and can contain duplicate elements
+    tuples are represented by () round braces
+    tuples are immutable or unchangable
+'''
+tuple = ('oneT','twoT','ThreeT','FourT')
+print(tuple)
+# tuple[4] = 'Neon'
+# Tuples cannnot be altered once they are Declared
+print(tuple)
+
+'''
+    Tuples can also be declared by Tuple constructior tuple()
+    len() function returns the number of elements in the tuple
+'''
+tuplex = (("Neon Genesis Evangelion","Shinji Ikari","Misato Kasturagi","Rey Ayanami"))
+print(tuplex[0])
