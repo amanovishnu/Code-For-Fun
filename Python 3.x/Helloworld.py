3571d526f2583b45e87758b2f0c558bda5caf543
Mano Vishnu <amanovishnu@gmail.com>
2018-06-16 17:19:17 +0530

    python notes

---
+++ Python 3.x/Helloworld.py
@@ -0,0 +1,48 @@
+# Python is a General Purpose High Level Programming Language
+# python was Created by Guido Van Rossum in the Early 90's when he was Working in National Research Institute Netherlands
+# the First offical Release of python was made on 13th feb 1991
+# python comments begin with pound or hash symbol
+print ("Guido Van Rosumm is the Father of Python ?")
+print("Hello World")
+# in other programming languages indentation is for Readablity only where as in python it is used to identify a block of code
+# python uses indentation to identify a block of code
+if 5>2 :
+    print("true")
+else:
+    print("False")
+''' This is a Python Docstring or Multi line Comment in Python'''
+""" Python Multiline Comment can also use Double Quotes as Comment Symbols """
+# Pound or Hash # symbol is used as Single line Comment in Python
+# in Python Comments are also called docstrings
+# in python variables are declared using the variable name there is no need to specify the data type for the variable
+# python is a forgiving language interms of data types
+x = 5
+print(x)
+# in python statements dont end with semicolons where as in other programming langugaes statements end with semicolons
+x = 10  
+print(x)
+name = "geekymano"
+print(name)
+name = "this repo belongs to geekymano"
+print(name)
+''' Rules for variable names
+    the variable name can contain letters, alphabets and it can also use special characters like underscore
+    the variable names can begin with letters only it cannot begin with number or underscore
+    in python variable names are case sensitive similar to other programming languages
+    Example age and AGE are Different variables in python
+'''
+name = "geekymano"
+print (name)
+# python uses + symbol or concatenation operator to join a string and string or a variable and a string
+print(name+"is a Geek")
+print(name + "is a Machine Learning Enthusiast")
+# in Python we can use concatenation operator to also join a string and a string
+print("geekymano"+" "+"owns this Repository")
+print("Python "+"is "+"a "+"General "+"Purpose "+"Programming "+"Language")
+print("Pyton"+" "+"was"+" "+"Created"+" "+"by"+" "+"Guido Van Rossum")
+# + operator when used with a string and varaiable or string and string joins them into one single string whereas when used with two numbers add them
+x= 15554654
+print(x)
+print(x+456798798)
+x = "mano"
+print(x+5)
