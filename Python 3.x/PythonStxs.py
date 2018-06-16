c4cb2fe91721e1b2ffdb357dcab82b989b5b75db
Mano Vishnu <amanovishnu@gmail.com>
2018-06-16 22:36:18 +0530

    Python set notes completed 16062018

---
+++ Python 3.x/PythonStxs.py
@@ -0,0 +1,25 @@
+# First Pubic Release of Python was made in 1991 4 years before java
+'''
+    sets is a collection of unordered unindexed elements
+    theys are written inside curly braces
+'''
+# set = {'apple','banana','cherry'}
+# print(set)
+'''
+    sets can aslo be declared with set() constructor 
+'''
+sest = set(("Mano","Alaska","IS"))
+print(sest)
+'''
+    to add new elements to set use add() method
+'''
+sest.add("Evangrlion")
+print(sest)
+'''
+    to remove elements from  set use remove() method
+'''
+sest.remove('Mano')
+print(sest)
+'''
+    use the len() function to find the number of elements in set
+'''
