11525fb318132ad825a05a2032c9cb741df10e07
Mano Vishnu <amanovishnu@gmail.com>
2018-06-17 08:33:57 +0530

    Python-if-Conditional

---
+++ Python 3.x/Conditionals.py
@@ -0,0 +1,35 @@
+# Unlike other Languages which uses indentation for readablity python uses indentation for defining the scope of code
+'''
+    Comparison Operators
+    Eqauls ==
+    not Equal != <>
+    greater than >
+    less than <
+    greater tha  equal to >=
+    less than equal to <=
+'''
+num1 = 200
+num2 = 455
+if num1<num2 :
+    print("Aloha Maths is Shit")
+else :
+    print("No Way Maths Still Sucks")
+'''
+    conditional in pythons contain
+    if
+    elif
+    else
+'''
+a = 45
+b = 25
+if a < b :
+    print("A is Lesser than B")
+elif a == b :
+    print("A is Equal to B")
+else :
+    print("A is Greater than B")
+# Python Notes on If Conditional Completed
+'''
+    Author: geekymano
+    Date: 17062018
+'''
