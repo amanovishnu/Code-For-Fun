d349c8fc992b7c00acd527ec48837df686d24433
Mano Vishnu <geekymano@gmail.com>
2019-01-21 12:16:12 +0530

    WorkDoc Updated

--- WorkDoc/WorkDoc.c
+++ WorkDoc/WorkDoc.c
@@ -7,7 +7,8 @@ struct geeksforgeeks {
 };
 int main(void) {
     struct geeksforgeeks Hello;
-    Hello = {"Geeks","For","City",25};
+    Hello.name = 'Chalk';
+    
     printf("%d",Hello.pin);
     return 0;
 }
