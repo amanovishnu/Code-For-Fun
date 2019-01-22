36d5c485b3359ab3887811ddeb77f4c53b6ad249
geekymano <amanovishnu@gmail.com>
2019-01-22 13:20:56 +0530

    Structures Explained

--- WorkDoc/WorkDoc.c
+++ WorkDoc/WorkDoc.c
@@ -1,4 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+struct geeksforgeeks {
+    char name[10];
+    char street[30];
+    char city[50];
+    int pin;
+};
 int main(void) {
-
+    struct geeksforgeeks Hello;
+    Hello.name = {'Chalk'};
+    
+    printf("%d",Hello.pin);
+    return 0;
 }
